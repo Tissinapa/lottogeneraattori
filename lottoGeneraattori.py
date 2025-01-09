1f591b6549c2680c4712824dc00afccc54649502
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2025-01-09 19:37:03 +0200

    added user selected iteration and brute force win

--- lottoGeneraattori.py
+++ lottoGeneraattori.py
@@ -138,10 +138,79 @@ class LotteryGame:
             print(f'Annoit numerot {self.numbers} ja lisänumerot {self.additionalNumbers}\n')       
         return    
 
-    def simulateWinningNumbers(self):
+    def simulateWinningNumbers(self, iterations):
+        correct_numbers = 0
+        correct_additional_numbers = 0
+        if not self.numbers and self.additionalNumbers:
+            print("Anna ensin numerot")
+            return
+        print(f'Simuloidaan {iterations} rivin voittonumerot.....\n')
         
-        pass
+        
+        for _ in range(iterations):
+            # Lotto
+            if self.gameMode == None or self.gameMode == '1':
+                gen_random_numb = random.sample(range(1,31),7)
+                gen_rand_addi_numb = random.sample(range(1,31),1)
+            # Vikinglotto
+            elif self.gameMode == '2':
+                gen_random_numb = random.sample(range(1,49),6)
+                gen_rand_addi_numb = random.sample(range(1,49),1)
+            # Eurojackpot
+            elif self.gameMode == '3':
+                gen_random_numb = random.sample(range(1,50),5)
+                gen_rand_addi_numb = random.sample(range(1,50),2)
+        
+        
+        for num in gen_random_numb:
+            if num in self.numbers:
+                correct_numbers += 1
+        
+        for addi_num in gen_rand_addi_numb:
+            if addi_num in self.additionalNumbers:
+                correct_additional_numbers += 1
+        
+        print(f"Simulaation aikana {iterations} rivistä löytyi:")
+        print(f"- Oikein osuneita päänumeroita: {correct_numbers}")
+        print(f"- Oikein osuneita lisänumeroita: {correct_additional_numbers}\n")
+        
+        return 
+    def bruteforceWin(self):
+        
+        attempts = 0  # Laskee yritykset
 
+        print("Etsitään niin kauan kunnes saat kaikki oikein, tässä voi mennä hetki...\n")
+
+        while True:
+            attempts += 1
+
+            # Lotto
+            if self.gameMode == None or self.gameMode == '1':  # Lotto
+                gen_random_numb = random.sample(range(1, 31), 7)
+                gen_rand_addi_numb = random.sample(range(1, 31), 1)
+
+            elif self.gameMode == '2':  # Vikinglotto
+                gen_random_numb = random.sample(range(1, 49), 6)
+                gen_rand_addi_numb = random.sample(range(1, 49), 1)
+
+            elif self.gameMode == '3':  # Eurojackpot
+                gen_random_numb = random.sample(range(1, 51), 5)
+                gen_rand_addi_numb = random.sample(range(1, 13), 2)
+
+            else:
+                print("Pelitilaa ei ole valittu.")
+                return
+
+            
+            main_match = set(gen_random_numb) == set(self.numbers)
+            additional_match = set(gen_rand_addi_numb) == set(self.additionalNumbers)
+
+           
+            if main_match and additional_match:
+                print(f"Kaikki numerot osuivat oikein! Voiton saaminen kesti {attempts} yritystä.")
+                print(f"Voittorivi: {gen_random_numb} ja lisänumerot: {gen_rand_addi_numb}\n")
+                break
+        return
 def menu():
     
     print("1) Generoi sekalaiset numerot")
@@ -160,6 +229,7 @@ def main():
     game = LotteryGame()
     
     user_input = None
+    iterations = 0
     while(user_input != 0):
         user_input = menu()
         if user_input == "1":
@@ -167,9 +237,10 @@ def main():
         elif user_input == "2":
             game.selectOwnNumbers()
         elif user_input == "3":
-            game.simulateWinningNumbers()
+            iterations = int(input("Monta riviä arvotaan? "))
+            game.simulateWinningNumbers(iterations)
         elif user_input == "4":
-            pass
+            game.bruteforceWin()
         elif user_input == "5":
             game.gameModes()
         elif user_input == "0":
